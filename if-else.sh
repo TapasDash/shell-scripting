c93ac891795dd75e815be3e5caeac34481fb283d
TapasDash <tapasdash017@gmail.com>
2020-01-20 13:32:12 +0530

    Create if-else.sh

---
+++ if-else.sh
@@ -0,0 +1,11 @@
+number=10
+if [ $number -eq 10 ]number=11
+if [ $number -eq 10 ]
+then 
+    echo "a is equal to 10"
+else
+    echo "a is not equal to 10"
+fi
+then 
+    echo "a is equal to 10"
+fi
